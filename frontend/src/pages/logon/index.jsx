9205ee28fee05c50110b628a2212301e7e6613d7
evertonsse <evertonsse@outlook.com>
2023-07-12 00:41:01 -0300

    ok

--- frontend/src/pages/logon/index.jsx
+++ frontend/src/pages/logon/index.jsx
@@ -1,18 +1,29 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from "react-icons/fi";
 import restaurantImg  from '../../assets/restaurant.png'
 import './styles.css';
 
 export default function Logon() {
+	const [id, setId] = useState('')
+	const [pass, setPass] = useState('')
+	const history = useHistory()
+
+	async function handleLogin(e) { 
+		
+
+		e.preventDefault()
+	} 
+
 	return (
 		<div className="logon-container">
 			<section className="form">
 				<p>Comanda <br></br>Online</p>
-				<form>
+				<form onSubmit={handleLogin}>
 					<h1>Faça seu Logon</h1>
-					<input placeholder="Sua ID" />
+					<input  id= "id" placeholder="Sua ID" />
+					<input id= "pass" placeholder="Senha" />
 					<button className="button validate" type="submit">
 						Entrar
 					</button>
