963c451c35ff918a9d53c686ec8bc1820514f8a8
evertonsse <evertonsse@outlook.com>
2023-07-12 21:28:24 -0300

    ok

--- backend/src/controllers/employeesController.js
+++ backend/src/controllers/employeesController.js
@@ -1,4 +1,7 @@
+
 const employee = require('../models/employees')
+const cryptonite = require('../utils/cryptonite')
+require('dotenv').config()
 
 module.exports = {
 	async login(request, response) {
@@ -10,11 +13,25 @@ module.exports = {
 				pass,
 			},
 		})
-        if (user){ 
-            return response.json(user)
-        } else {
-            return response.json({error: 'User not found'})
-        }
+		if (user) {
+			return response.json(user)
+		} else {
+			return response.json({ error: 'User not found' })
+		}
+	},
 
-	}
+	async create(request, response) {
+		const { name, email, pass } = request.body
+
+		const hash = cryptonite.crypt(pass, process.env.CRYPTO_KEY )
+
+		if (await employee.create({name, email, pass : hash})) { 
+			return response.json({ message: 'Employee created' })
+		} else  { 
+			return response.json({ error: 'Employee not created' })
+		}
+
+	},
+
+	
 }
